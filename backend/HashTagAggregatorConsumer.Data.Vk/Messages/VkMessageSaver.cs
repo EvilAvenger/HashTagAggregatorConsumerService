a8a08659ba535791c339ac86a22561e8ee281d5b
vbibikov <evilavenger@yandex.ru>
2017-08-24 15:51:04 +0300

    Move request to Social networks into separate services Fixes EvilAvenger/HashtagAggregator#25

--- backend/HashTagAggregatorConsumer.Data.Vk/Messages/VkMessageSaver.cs
+++ backend/HashTagAggregatorConsumer.Data.Vk/Messages/VkMessageSaver.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Threading.Tasks;
 using HashtagAggregator.Core.Contracts.Interface.Cqrs.Command;
+using HashtagAggregator.Core.Entities.VkEntities;
 using HashtagAggregatorConsumer.Contracts.Interface.Messages;
+using HashTagAggregatorConsumer.Data.Vk.Mappers;
+using HashTagAggregatorConsumer.Queries.Cqrs.Results.Commands;
 using MediatR;
 using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
 
 namespace HashTagAggregatorConsumer.Data.Vk.Messages
 {
-    public class VkMessageSaver: IMessageSaver
+    public class VkMessageSaver : IMessageSaver
     {
         private readonly IMediator mediator;
 
@@ -16,9 +20,22 @@ namespace HashTagAggregatorConsumer.Data.Vk.Messages
             this.mediator = mediator;
         }
 
-        public Task<ICommandResult> Save(CloudQueueMessage message)
+        public async Task<ICommandResult> Save(CloudQueueMessage message)
         {
-            throw new NotImplementedException();
+            var feed = JsonConvert.DeserializeObject<VkNewsFeed>(message.AsString);
+            var mapper = new VkMessageResultMapper();
+            var results = mapper.MapBunch(feed);
+            var commandResult = new CommandResult
+            {
+                Success = true
+            };
+            foreach (var command in results)
+            {
+                var result = await mediator.Send(command);
+                commandResult.Success = commandResult.Success && result.Success;
+                commandResult.Message += result.Message;
+            }
+            return commandResult;
         }
     }
 }
